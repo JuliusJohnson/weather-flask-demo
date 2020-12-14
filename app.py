47569b2a22a912ac109a4153415760bf2f8a1d0f
Julius <contact@juliusj.com>
2020-12-13 22:15:40 -0500

    Fixed Long/lat & added state field

--- app.py
+++ app.py
@@ -14,27 +14,34 @@ def weather():
     api = development.config.openweatherapi
     if request.method == 'POST':
         city = request.form['city']
+        state = request.form['state']
     else:
         city = 'Atlanta' #default city
+        state = 'Georgia' #default state
 
-    r = rqst.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}')
+    r = rqst.get(f'https://api.openweathermap.org/data/2.5/weather?q={city},{state}&appid={api}')
+
+    print(r)
 
     list_of_data = r.json()
-    lat = list_of_data['coord']['lon'] #gets longitude from inputed city
-    lon = list_of_data['coord']['lat']#gets latitude from inputed city
+    print(list_of_data)
+    lon = list_of_data['coord']['lon'] #gets longitude from inputed city
+    lat = list_of_data['coord']['lat']#gets latitude from inputed city
     more_weather_data = rqst.get(f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={api}') #pulls data from openweathermap.org's one call api
     detail_weather_data = more_weather_data.json()
+    
     data = {
+        "city": str(city),
         "weather_description": str(detail_weather_data['current']['weather'][0]), 
         "coordinate": str(list_of_data['coord']['lon']) + ' ' 
                     + str(list_of_data['coord']['lat']),
-        "w_icon": str("http://openweathermap.org/img/wn/" + str(detail_weather_data['current']['weather'][0]['icon']) + "@2x.png") ,
+        "w_icon": str("http://openweathermap.org/img/wn/" + str(detail_weather_data['current']['weather'][0]['icon']) + ".png") ,
         "temp": str(detail_weather_data['current']['temp']), #Converting to Farenheight
         "feel": str(detail_weather_data['current']['feels_like']), #Converting to Farenheight
         "mintemp": str(detail_weather_data['daily'][0]['temp']['min']), 
         "maxtemp": str(detail_weather_data['daily'][0]['temp']['max']), 
     } 
-    pprint(detail_weather_data['current'])
+    #pprint(detail_weather_data['current'])
     return render_template('index.html', data=data)
 
     if __name__ == '__main__':
